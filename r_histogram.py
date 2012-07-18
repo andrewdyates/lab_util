24ee354e8d76e8e11abee2f7507aa76f09c9d1e5
Andrew D. Yates <andrewyates.name@gmail.com>
2012-07-17 20:24:40 -0400

    histogram plotter

---
+++ r_histogram.py
@@ -0,0 +1,28 @@
+import sys, os
+import numpy as np
+
+from rpy2 import robjects
+from rpy2.robjects.packages import importr
+import rpy2.robjects.numpy2ri
+rpy2.robjects.numpy2ri.activate()
+
+r_base = importr('base')
+grdevices = importr('grDevices')
+stats = importr('stats')
+graphics = importr('graphics')
+
+
+def main(npyfname, outdir):
+  M = np.load(npyfname)
+
+  # Draw plot
+  plot_pdfname = os.path.join(outdir, "%s_hist.pdf" % os.path.basename(npyfname))
+  print "Plotting %s..." % plot_pdfname
+  grdevices.pdf(plot_pdfname)
+  graphics.hist(M, prob=True, main=os.path.basename(npyfname), xlab="score", col="grey")
+  graphics.lines(stats.density(M), color="blue")
+  graphics.rug(M, col="black")
+  grdevices.dev_off()
+
+if __name__ == "__main__":
+  main(**dict([s.split('=') for s in sys.argv[1:]]))
