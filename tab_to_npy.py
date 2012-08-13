35da2d7d1f03bc5f4e9b8d2d001bdc33ee7c72e1
Andrew Yates <andrewyates.name@gmail.com>
2012-08-13 16:49:03 -0400

    documentation for tab_to_npy

--- tab_to_npy.py
+++ tab_to_npy.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 import numpy as np
-import os
 
 # http://docs.scipy.org/doc/numpy/reference/generated/numpy.genfromtxt.html#numpy.genfromtxt
 # http://docs.scipy.org/doc/numpy/reference/generated/numpy.loadtxt.html#numpy.loadtxt
 
 def name_iter(fp, varlist):
+  """Yield next row of floats w/o ID in first column; append ID to varlist.
+
+  Args:
+    fp: [*str] of readable file-pointer-like iterator, yields tab delimited line
+    varlist: [str] of row IDs to append to
+  """
   for line in fp:
     if line[0] in ('#','\n'): continue
     name,c,row = line.partition('\t')
@@ -13,6 +18,13 @@ def name_iter(fp, varlist):
     yield row
 
 def tab_to_npy(tab_fname):
+  """Return masked numpy array and in-order variable list from row-labeled .tab matrix.
+
+  Args:
+    tab_fname: str of path to .tab plain text matrix of floats; first column is row ID
+  Returns:
+    (np.MaskedArray, [str]) of float data and list of row IDs in order
+  """
   varlist = []
   M = np.genfromtxt(name_iter(open(tab_fname), varlist), usemask=True, delimiter='\t')
   return M, varlist
